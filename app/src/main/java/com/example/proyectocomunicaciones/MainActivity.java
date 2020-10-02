1fe8e10b22f1b77e28fefeba608dac7f5bb219b7
JhonierCordoba <jhonier.cordoba@udea.edu.co>
2020-10-02 08:39:36 -0500

    Saca la info de cada materia

--- app/src/main/java/com/example/proyectocomunicaciones/MainActivity.java
+++ app/src/main/java/com/example/proyectocomunicaciones/MainActivity.java
@@ -6,18 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.ListIterator;
+
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<String> telefonos=new ArrayList<String>();
+    ArrayList<String> materias=new ArrayList<String>();
     ArrayAdapter<String>  adaptador;
+    ArrayList<Integer> listaCreditos=new ArrayList<Integer>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void BotonRevisarMaterias(View view){
         setContentView(R.layout.materias);
-        adaptador=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,telefonos);
+        adaptador=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,materias);
         ListView listaMaterias = (ListView)findViewById(R.id.ListaMaterias);
         listaMaterias.setAdapter(adaptador);
         listaMaterias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -39,21 +41,26 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Object object = parent.getAdapter().getItem(position);
                 Toast.makeText(getBaseContext(), object.toString(), Toast.LENGTH_SHORT).show();
+                setContentView(R.layout.materias_info);
+                TextView materia = (TextView) findViewById(R.id.materia);
+                materia.setText(object.toString());
+                TextView creditos = (TextView) findViewById(R.id.creditos);
+                creditos.setText(listaCreditos.get(position).toString());
             }
         });
-
     }
 
 
     public void agregarMateria(View v) {
-        EditText text = (EditText) findViewById(R.id.NombreMateria);
-        telefonos.add(text.getText().toString());
-        adaptador.notifyDataSetChanged();
-        text.setText("");
-    }
-
-    public void BotonGuardarMateria(){
-        setContentView(R.layout.crear_materia);
+        EditText nombreMateria = (EditText) findViewById(R.id.NombreMateria);
+        EditText cantidadCreditos = (EditText) findViewById(R.id.CantidadCreditos);
+        materias.add(nombreMateria.getText().toString());
+        String texto = cantidadCreditos.getText().toString();
+        int num =Integer.parseInt(texto);
+        listaCreditos.add(num);
+        //adaptador.notifyDataSetChanged();
+        nombreMateria.setText("");
+        cantidadCreditos.setText("");
     }
 
     public void BotonVolver(View view){
